e66a8e70657790fb37c2eebfc73e14a3e1c56e99
Thomas Kägi <kaegit@gmail.com>
2019-09-27 11:38:05 +0200

    Add a 'first' parameter to products query.

--- GraphQl/ProductQuery.cs
+++ GraphQl/ProductQuery.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 using GraphQL.Types;
 using productsWebapi.GraphQl.Types;
 using productsWebapi.Products;
@@ -11,15 +13,20 @@ namespace productsWebapi.GraphQl
     {
         const String nameArg = "name";
         const String typeArg = "type";
+        const String firstArg = "first";
+
+        private static readonly IEnumerable<IProduct> NO_PRODUCTS = new IProduct[0];
         private readonly IRepository<IProduct> _repo;
         public ProductQuery(IRepository<IProduct> repo)
         {
             _repo = repo;
-            var args = new QueryArguments(
+            var productArgs = new QueryArguments(
                 new QueryArgument<NonNullGraphType<StringGraphType>>{Name = nameArg},
                 new QueryArgument<StringGraphType>{Name = typeArg});
-            Field<ProductInterface>("product", arguments: args, resolve: ResolveProduct);
-            Field<ListGraphType<ProductInterface>>("products", resolve: _ => _repo.All());
+            var productsArgs = new QueryArguments(
+                new QueryArgument<IntGraphType>{Name = firstArg, DefaultValue=-1});
+            Field<ProductInterface>("product", arguments: productArgs, resolve: ResolveProduct);
+            Field<ListGraphType<ProductInterface>>("products", arguments: productsArgs, resolve: ResolveProducts);
         }
 
         private IProduct ResolveProduct(ResolveFieldContext<Object> context)
@@ -31,6 +38,16 @@ namespace productsWebapi.GraphQl
                 products = products.Where(product => product.Type == type);
             }
             return products.FirstOrDefault();
+        }
+
+        private async Task<IEnumerable<IProduct>> ResolveProducts(ResolveFieldContext<Object> context)
+        {
+            var first = context.GetArgument<Int32>(firstArg);
+            if(first == 0){
+                return NO_PRODUCTS; 
+            }
+            var all = await _repo.All().ConfigureAwait(false);
+            return first < 0 ? all : all.Take(first);
         }
     }
 }
