c99f1845a7e9ac66cae8c16613c1c857d0cbdfb9
Thomas Kägi <kaegit@gmail.com>
2019-10-15 10:09:40 +0200

    Fix code duplication of interface defs.

--- GraphQl/Types/ProductInterface.cs
+++ GraphQl/Types/ProductInterface.cs
@@ -1,18 +1,25 @@
+using System;
 using GraphQL.Types;
 using productsWebapi.Products;
 
 namespace productsWebapi.GraphQl.Types
 {
-    public class ProductInterface: InterfaceGraphType<IProduct>
+    public sealed class ProductInterface: InterfaceGraphType<IProduct>
     {
-        public ProductInterface()
+        public ProductInterface() 
         {
             Name = nameof(Product);
-            Field(p => p.Name).Description("The products name");
-            Field(p => p.Id).Description("The products id").Type(new NonNullGraphType(new IdGraphType()));
-            Field(p => p.Stock).Description("The number of products in stock");
-            Field(p => p.Type).Description("The type of product");
-            Field<ListGraphType<ReviewType>>("reviews");
+            Register<IProduct>(this);
+        }
+
+        internal static void Register<TProduct>(ComplexGraphType<TProduct> type, Func<ResolveFieldContext<TProduct>, Object> reviewResolver=null)
+            where TProduct: IProduct
+        {
+            type.Field(p => p.Name).Description("The products name");
+            type.Field(p => p.Id).Description("The products id").Type(new NonNullGraphType(new IdGraphType()));
+            type.Field(p => p.Stock).Description("The number of products in stock");
+            type.Field(p => p.Type).Description("The type of product");
+            type.Field<ListGraphType<ReviewType>>("reviews", resolve: reviewResolver);
         }
     }
 }
